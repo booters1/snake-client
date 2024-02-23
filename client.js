1eb0c0910270f5d022679d037699b3898f3a2f1e
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-23 10:55:54 -0700

    worked on move up hard coded // out

--- client.js
+++ client.js
@@ -10,9 +10,16 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // Event handler for incoming data
+  // event handler for incoming data
   conn.on("data", (data) => {
-    console.log("Server says:", data); // Log incoming data from the server
+    console.log("Server says:", data); // log that data
+  });
+  
+  // event handler for a successful connection
+  conn.on("connect", () => {
+    console.log("successfully connected to game server");
+    conn.write("Name: RH1"); // Writes name to server
+    //conn.write("Move: UP"); 
   });
 
   return conn;
