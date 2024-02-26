2655a51f0297ceef1d8620fdae624c257de7da9c
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-26 12:04:23 -0700

    added movement key logic

--- input.js
+++ input.js
@@ -12,6 +12,14 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') { // ctrl + c
     process.exit(); 
+  } else if (key === 'w') { 
+    console.log("Move: UP 🔼");
+  } else if (key === 'a') {
+    console.log("Move: LEFT ◀")
+  } else if (key === 'd') {
+    console.log("Move: RIGHT ▶")
+  } else if (key === 's') {
+    console.log("Move: DOWN 🔽")
   }
 };
 
